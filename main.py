08ac00fd260dfb9fb44fe8c8ddee080299ba3383
ArtemiiD <artemiidudinov@gmail.com>
2023-07-03 18:19:48 +0500

    v2

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-a = input('пиши здесь:')
-print(a)
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot('6309899359:AAF0nGsudYSuFxHqTcpj7wWaNmLnsBvtX8k')
+@bot.message_handler(commands=['start'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Привет')
+@bot.message_handler(commands=['help'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'i cant help')
+@bot.message_handler(commands=['finish'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Пока')
+@bot.message_handler(func=lambda message: True)
+async def echo(message):
+    #await bot.reply_to(message, message.text)
+    if 'дела' or 'настроение'in message.text:
+        await bot.reply_to(message, 'Хорошо')
+import asyncio
+asyncio.run(bot.polling())
